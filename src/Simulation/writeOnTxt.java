35ffb6c235548defab8dcafbc80f6b8938852c2d
ionas fabris <ionasfabris3@gmai.com>
2020-05-14 11:53:57 +0200

    Added The writeONTxt class

---
+++ src/Simulation/writeOnTxt.java
@@ -0,0 +1,41 @@
+package Simulation;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import static java.lang.System.out;
+
+public class writeOnTxt {
+    public static void writeOnTxt(ArrayList<Time> arrivalTime, ArrayList<String> customerType, ArrayList<Time> finishTime) throws FileNotFoundException {
+        out.println("writing ");
+        PrintWriter writer = new PrintWriter(new File("MS.csv"));
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("customer type: ");
+        sb.append("arrival times: ");
+        sb.append("finishing times: ");
+        sb.append("\n");
+
+        try {
+            for (int i = 0; i < arrivalTime.size(); i++) {
+                sb.append(customerType.get(i));
+                sb.append(", ");
+                sb.append(arrivalTime.get(i));
+                sb.append(", ");
+                sb.append(finishTime.get(i));
+                sb.append("\n");
+            }
+        } catch (IndexOutOfBoundsException e) {
+            writer.write(sb.toString());
+            writer.close();
+        }
+
+        //out.println(sb.toString());
+        writer.write(sb.toString());
+        writer.close();
+
+    }
+}
