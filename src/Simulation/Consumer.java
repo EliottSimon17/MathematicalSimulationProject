da89f742612639943453b2a487c4473184919d66
Exodia7 <valentin.ringlet@gmail.com>
2020-05-14 14:30:26 +0200

    Added the previous modifications from Consumer into Corporate as well
    Improved their methods, and put the same methods in both classes
    Moreover, changed all doubles represeting time to Time objects

--- src/Simulation/Consumer.java
+++ src/Simulation/Consumer.java
@@ -5,20 +5,19 @@ package Simulation;
  *  - GIVE THE PARAMETERS OF THE POISSON AND TRUNCNORMAL DISTRIBUTIONS IN THE CONSTRUCTOR
  *      --> compute them from the assignment sheet
  *
- *  - CHANGE TO USE TIME OBJECTS INSTEAD OF A DOUBLE VALUE
- *
  *  - WRITE THE BODY OF THE getNewServiceTime() METHOD TO USE THE TRUNCNORMAL
  */
 
 public class Consumer extends Customer {
-    private Poisson arrivalDistr = new Poisson(param1, param2, ...);
-    private TruncNormal serviceDistr = TruncNormal(param1, param2);
+    // These variables are the distributions used for generating arrival times and service times
+    private static Poisson arrivalDistr = new Poisson(param1, param2, ...);
+    private static TruncNormal serviceDistr = TruncNormal(param1, param2);
 
     /**
      * Default constructor with no parameters
      */
     public Consumer() {
-        this(0);
+        this(new Time(0));
     }
 
     /**
@@ -26,16 +25,26 @@ public class Consumer extends Customer {
      *
      * @param previousArrivalTime the time of the last arrival which happened so far
      */
-    public Consumer(double previousArrivalTime) {
+    public Consumer(Time previousArrivalTime) {
+        // Call the superclass constructor (which should be the one from Product
+        super();
         // Set the previous arrival time
         arrivalDistr.setPreviousArrivalTime(previousArrivalTime);
     }
 
-    public static double getNewArrivalTime(double currentTime) {
-        // Give the current time
-        arrivalDistr.setCurrentTime(currentTime);
-        // Draw a random number
-        arrivalDistr.drawRandom();
+    /**
+     * This method generates a new arrival time given the current time.
+     *
+     * @param previousArrivalTime the previous generated arrival time
+     *
+     * @return a new arrival time, generated according to the arrivalDistr distribution
+     */
+    public static double getNewArrivalTime(Time previousArrivalTime) {
+        // Pass on the previous arrival time to the arrival time distribution
+        arrivalDistr.setPreviousArrivalTime(previousArrivalTime);
+
+        // Generate and return a random number
+        return arrivalDistr.drawRandom();
     }
 
     public static double getNewServiceTime() {
--- src/Simulation/Corporate.java
+++ src/Simulation/Corporate.java
@@ -1,19 +1,42 @@
 package Simulation;
 
-import java.util.Random;
 
-
-/* TODO CHANGE SIMILARLY TO CONSUMER.java */
-
+/* TODO:
+ *  - GIVE THE PARAMETERS OF THE POISSON AND TRUNCNORMAL DISTRIBUTIONS IN THE CONSTRUCTOR
+ *      --> compute them from the assignment sheet
+ *
+ *  - WRITE THE BODY OF THE getNewServiceTime() METHOD TO USE THE TRUNCNORMAL
+ */
 
 public class Corporate extends Customer {
-    private Poisson arrivalDistr = Poisson(param1, param2, ...);
-    private TruncNormal serviceDistr = TruncNormal(param1, param2);
+    // These variables are the distributions used for generating arrival times and service times
+    private static Poisson arrivalDistr = Poisson(param1, param2, ...);
+    private static TruncNormal serviceDistr = TruncNormal(param1, param2);
 
-    public Corporate () {
-        super();
-        arrivalDistr.setPreviousArrivalTime(0);
+    /**
+     * Default constructor with no parameters
+     */
+    public Corporate() {
+        this(new Time(0));
     }
+
+    /**
+     * Parametric constructor
+     *
+     * @param previousArrivalTime the time of the last arrival which happened so far
+     */
+    public Corporate (Time previousArrivalTime) {
+        // Call the superclass constructor (which should be the one from Product
+        super();
+        // Set the previous arrival time
+        arrivalDistr.setPreviousArrivalTime(previousArrivalTime);
+    }
+
+    /* COULD STILL BE USED FOR THE LAMBDAT OBJECT OF THE POISSON DISTRIBUTION,
+        BUT !
+            It needs to use a time object instead of a double object
+            And it should be used to create a LambdaT object ...
+                --> could be either an anonymous class object or a lambda expression
 
     public double getPoissonRandom(double time) {
         double ratePerMinutes = 0;
@@ -29,12 +52,21 @@ public class Corporate extends Customer {
 
         return time + Math.log(r.nextDouble())/-ratePerMinutes;
     }
+    */
 
-    public static double getNewArrivalTime(double currentTime) {
-        // Give the current time
-        arrivalDistr.setCurrentTime(currentTime);
-        // Draw a random number
-        arrivalDistr.drawRandom();
+    /**
+     * This method generates a new arrival time given the current time.
+     *
+     * @param previousArrivalTime the previous generated arrival time
+     *
+     * @return a new arrival time, generated according to the arrivalDistr distribution
+     */
+    public static double getNewArrivalTime(Time previousArrivalTime) {
+        // Pass on the previous arrival time to the arrival time distribution
+        arrivalDistr.setPreviousArrivalTime(previousArrivalTime);
+
+        // Generate and return a random number
+        return arrivalDistr.drawRandom();
     }
 
     public static double getNewServiceTime() {
