c64ba8bd07978e482febc76694f57e7628112452
Exodia7 <valentin.ringlet@gmail.com>
2020-05-15 17:56:52 +0200

    Made some notes about how many of each object we will have in Simulation.java

--- src/Simulation/Simulation.java
+++ src/Simulation/Simulation.java
@@ -14,22 +14,29 @@ public class Simulation {
     public Sink sink;
     public Machine mach;
 
+    // Recipe for a successful simulation:
+    //
+    // 1 single CEventList
+    // 2 different queues, one for Consumers and one for Corporate
+    // 2 different sources, one for Consumers (SourceConsumer) and one for Corporate (SourceCorporate)
+    // 1 single Sink
+    // as many machines as we want, depending on the number of CSA per shift
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	// Create an eventlist
+        // Create an eventlist
         CEventList l = new CEventList();
         // A queue for the machine
         Queue q = new Queue();
         // A source
-        Source s = new Source(q,l,"Source 1");
+        Source s = new Source(q, l, "Source 1");
         // A sink
         Sink si = new Sink("Sink 1");
         // A machine
-        Machine m = new Machine(q,si,l,"Machine 1");
+        Machine m = new Machine(q, si, l, "Machine 1");
         // start the eventlist
         l.start(new Time(2000)); // 2000 is maximum time
     }
-    
 }
