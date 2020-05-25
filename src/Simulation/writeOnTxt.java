63c0fc7b39034a6a85455f0a9550280e8787fae9
Exodia7 <valentin.ringlet@gmail.com>
2020-05-25 14:28:56 +0200

    For the almighty merge

--- src/Simulation/writeOnTxt.java
+++ src/Simulation/writeOnTxt.java
@@ -7,38 +7,56 @@ import java.util.ArrayList;
 
 import static java.lang.System.out;
 
-/*
-    This class displays how we could write out data to .txt files
-        --> to be able to save simulation data from Java and load it into Matlab
- */
+public class WriteOnTxt {
 
-public class writeOnTxt {
-    public static void writeOnTxt(ArrayList<Time> arrivalTime, ArrayList<String> customerType, ArrayList<Time> finishTime) throws FileNotFoundException {
+    public static void writeOnTxt(ArrayList<Sink> sinks) throws FileNotFoundException {
+        out.println(sinks.get(0).getEvents().length);
+
         out.println("writing ");
-        PrintWriter writer = new PrintWriter(new File("MS.csv"));
+        PrintWriter writer = new PrintWriter(new File("MS.txt"));
 
         StringBuilder sb = new StringBuilder();
 
-        sb.append("customer type: ");
-        sb.append("arrival times: ");
-        sb.append("finishing times: ");
-        sb.append("\n");
+        // TODO make sure the times are in a fixed unit (seconds seems good ?)
 
-        try {
-            for (int i = 0; i < arrivalTime.size(); i++) {
-                // TODO make sure the times are in a fixed unit (seconds seems good ?)
-                sb.append(customerType.get(i));
-                sb.append(", ");
-                sb.append(arrivalTime.get(i));
-                sb.append(", ");
-                // TODO add the time when the Customer starts being processed by a Machine
-                // TODO add which CSA type the Customer was processed by
-                sb.append(finishTime.get(i));
-                sb.append("\n");
+        // TODO add the time when the Customer starts being processed by a Machine
+        // TODO add which CSA type the Customer was processed by
+
+        int counter = 0;
+        for (Sink s : sinks) {
+            sb.append("simulation: ").append(counter++).append("\n");
+
+            sb.append("Number: ");
+            sb.append("customer type: ");
+            sb.append("event: ");
+            sb.append("station: ");
+            sb.append("time: ");
+            sb.append("\n");
+
+            int[] numbers = s.getNumbers();
+            String[] customer = s.getProduct();
+            String[] events = s.getEvents();
+            String[] stations = s.getStations();
+            Time[] times = s.getTimes();
+
+
+            try {
+                for (int i = 0; i < times.length; i++) {
+                    sb.append(numbers[i]);
+                    sb.append(", ");
+                    sb.append(customer[i]);
+                    sb.append(", ");
+                    sb.append(events[i]);
+                    sb.append(", ");
+                    sb.append(stations[i]);
+                    sb.append(", ");
+                    sb.append(times[i].toSeconds());
+                    sb.append("\n");
+                }
+            } catch (IndexOutOfBoundsException e) {
+                writer.write(sb.toString());
+                writer.close();
             }
-        } catch (IndexOutOfBoundsException e) {
-            writer.write(sb.toString());
-            writer.close();
         }
 
         //out.println(sb.toString());
